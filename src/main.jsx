1274fa7baa3ef5660cef9b7686ccf404dc8d3a19
ragul66 <ragul1.gokul2.3@gmail.com>
2024-06-28 12:10:36 +0530

    bot Sakthi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import AddNamelist from "./Pages/AddNamelist";
+import AddCourse from "./Pages/Course";
+import Namelist from "./Pages/Namelist";
+// import AddMarks from "./Pages/AddMarks";
+import AddCourses from "./Pages/AddCourses";
+import Login from "./Pages/Login";
+import PtLists from "./Pages/PtList";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <HashRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/namelist" element={<Namelist />} />
+          <Route path="/namelist/:title" element={<AddNamelist />} />
+          <Route path="/course" element={<AddCourse />} />
+          <Route path="/course/:title" element={<AddCourses />} />
+          {/* <Route path="/addmarks" element={<AddMarks />} /> */}
+          <Route path="/pt" element={<PtLists />} />
+        </Routes>
+      </HashRouter>
   </React.StrictMode>,
 )
